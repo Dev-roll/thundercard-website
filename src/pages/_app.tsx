f169985b810011311fe487c2c835705618524298
keigomichi <hundredacrewood1165g7@gmail.com>
2022-11-20 02:25:28 +0900

    chore: 配色テーマを設定
    M2ベースで配色テーマ(light/dark)のパレットを作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,19 @@
+import CssBaseline from '@mui/material/CssBaseline'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import type { AppProps } from 'next/app'
+import { useMemo } from 'react'
+import { getDesignTokens } from 'theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // TODO: #8 light/dark モードを変更可能にする
+  const mode = 'dark'
+  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Component {...pageProps} />
+      </CssBaseline>
+    </ThemeProvider>
+  )
 }
